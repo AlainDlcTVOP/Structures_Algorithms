39696fa0a846c6b0c8a589b709a24c7b8ffbe2df
AlainDlcTVOP <alain_dlc@outlook.com>
2020-10-11 18:15:35 +0200

    Stack algo clear V3

--- Section_2/stack.c
+++ Section_2/stack.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define SIZE 10
+#define SIZE 5
 
 typedef struct
 {
@@ -8,77 +7,50 @@ typedef struct
     int top;
 } Stack;
 
-// prototype declarations
-void init(Stack *);
 void push(Stack *, int);
 int pop(Stack *);
+void init(Stack *);
 
+int main()
+{
+    Stack s1;
+
+    push(&s1, 100);
+    push(&s1, 300);
+    push(&s1, 500);
+    push(&s1, 170);
+
+    printf("deleted %d \n", pop(&s1));
+    printf("deleted %d \n", pop(&s1));
+    printf("deleted %d \n", pop(&s1));
+    printf("deleted %d \n", pop(&s1));
+
+    return 0;
+}
 void push(Stack *sp, int value)
 {
     if (sp->top == SIZE - 1)
     {
-        printf("Stack overflow\n");
+        printf("STACK OVERFLOW");
         return;
     }
-
     sp->top++;
     sp->item[sp->top] = value;
 }
-
 int pop(Stack *sp)
 {
     if (sp->top == -1)
     {
-        printf("Stack Underflow\n");
+        printf("STACK UNDERFLOW");
         return -9999;
     }
+
     int value;
     value = sp->item[sp->top];
     sp->top--;
     return value;
 }
-
 void init(Stack *sp)
 {
     sp->top = -1;
-}
-
-int main()
-{
-    Stack s1;
-    init(&s1);
-
-    printf("1. Push\n");
-    printf("2. Pop\n");
-    printf("3. Exit\n");
-    int choice, value;
-
-    while (1)
-    {
-        printf("Enter choice: ");
-        scanf("%d", &choice);
-
-        switch (choice)
-        {
-        case 1:
-            printf("Enter value: ");
-            scanf("%d", &value);
-            push(&s1, value);
-
-            break;
-        case 2:
-            value = pop(&s1);
-            if (value != -9999)
-            {
-                printf("Popped data: %d\n", value);
-            }
-            break;
-        case 3:
-            exit(0);
-        default:
-            printf("Invalid choice\n");
-        }
-    }
-
-    return 0;
 }
--- Section_2/stackdyn.c
+++ Section_2/stackdyn.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//#define SIZE 2
+
 typedef struct
 {
     int *item;
@@ -8,62 +10,18 @@ typedef struct
     int size;
 } Stack;
 
-// prototype declarations
-void init(Stack *, int size);
 void push(Stack *, int);
 int pop(Stack *);
-void dellocate(Stack *);
-
-void push(Stack *sp, int value)
-{
-    if (sp->top == sp->size - 1)
-    {
-        printf("Stack overflow\n");
-        return;
-    }
-
-    sp->top++;
-    sp->item[sp->top] = value;
-}
-
-int pop(Stack *sp)
-{
-    if (sp->top == -1)
-    {
-        printf("Stack Underflow\n");
-        return -9999;
-    }
-    int value;
-    value = sp->item[sp->top];
-    sp->top--;
-    return value;
-}
-
-void init(Stack *sp, int size)
-{
-    sp->top = -1;
-    sp->item = (int *)malloc(sizeof(int) * size);
-    if (sp->item == NULL)
-    {
-        printf("Unable to allocate memory\n");
-        exit(1);
-    }
-    sp->size = size;
-}
-
-void dellocate(Stack *sp)
-{
-    if (sp->item != NULL)
-        free(sp->item);
-}
-int main()
+void init(Stack *, int);
+void deallocate(Stack *);
+void menu();
+int main(void)
 {
     Stack s1;
-    init(&s1, 5);
+    init(&s1, 3);
 
-    printf("1. Push\n");
-    printf("2. Pop\n");
-    printf("3. Exit\n");
+    menu();
+
     int choice, value;
 
     while (1)
@@ -87,7 +45,8 @@ int main()
             }
             break;
         case 3:
-            dellocate(&s1);
+            printf("EXIT THE STACK PROGRAM\n");
+            deallocate(&s1);
             exit(0);
         default:
             printf("Invalid choice\n");
@@ -95,4 +54,52 @@ int main()
     }
 
     return 0;
+}
+
+void menu()
+{
+    puts("-------------");
+    printf(".1 PUSH\n");
+    printf(".2 POP\n");
+    printf(".3 EXIT\n");
+    puts("-------------");
+}
+void push(Stack *sp, int value)
+{
+    if (sp->top == sp->size - 1)
+    {
+        printf("OVERFLOW\n");
+        return;
+    }
+    sp->top++;
+    sp->item[sp->top] = value;
+}
+int pop(Stack *sp)
+{
+    if (sp->top == -1)
+    {
+        printf("UNDERFLOW\n");
+        return -9999;
+    }
+    int value;
+    value = sp->item[sp->top];
+    sp->top--;
+    return value;
+}
+void init(Stack *sp, int size)
+{
+    sp->top = -1;
+    sp->item = (int *)malloc(sizeof(int) * size);
+    if (sp->item == NULL)
+    {
+        printf("Cant't allocate memory\n");
+        exit(1);
+    }
+    sp->size = size;
+}
+void deallocate(Stack *sp)
+{
+    if (sp->item != NULL)
+
+        free(sp->item);
 }
