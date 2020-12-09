79b83ad311f8181a4c6bc8436c8a29ef4780aeae
AlainDlcTVOP <alain_dlc@outlook.com>
2020-12-09 20:08:50 +0100

    assigment completed 2020-12-09

---
+++ Repetition/w1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+
+    while (i <= printf("HI\n"))
+    {
+        printf("Hello\n");
+        i++;
+    }
+    printf("i = %d", i);
+    /*
+Hi = 3
+Hello
+HI
+Hello
+HI
+Hello
+HI
+*/
+    return 0;
+}
